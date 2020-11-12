f5c1bb906d2d9617bcb1e350369ed3fa1151af2a
alfian <alfian.setia100@gmail.com>
2020-11-12 09:09:50 +0700

    add test bot

---
+++ tes.py
@@ -0,0 +1,9 @@
+import telepot
+
+
+text = 'AIR HABIS'
+text1 = 'AIR TERSEDIA'
+chat_id = 635000156
+bot = telepot.Bot('1135612638:AAGh3d4sakQq945i_PWPUxlCLC4QdGL3lSM')
+bot.sendMessage(chat_id, text)
+
