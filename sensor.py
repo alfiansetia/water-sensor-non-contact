63e7b9354fb21a278373c81c2de709bc68342aed
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2020-11-28 13:13:10 +0700

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,9 +1,21 @@
-
+#Libraries
+import RPi.GPIO as GPIO
+import sys
+import telepot
+import time
+#IMPORT LIBRARY OS
+import os 
 import telepot
 
 
 text = 'AIR HABIS'
 text1 = 'AIR TERSEDIA'
-chat_id = 635000156
-bot = telepot.Bot('1135612638:AAGh3d4sakQq945i_PWPUxlCLC4QdGL3lSM')
-bot.sendMessage(chat_id, text)
+chat_id = CHATID
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BCM) # Use physical pin numberi
+
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+if GPIO.input(15) != GPIO.LOW :
+    bot = telepot.Bot('TOKEN')
+    bot.sendMessage(chat_id, text)
